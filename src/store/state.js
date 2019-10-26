50c106f3f19797476318a1d3be0d4fb7d59ca8e9
hojin-chu <cnghwls12@naver.com>
2019-10-26 17:43:55 +0900

    state update

--- src/store/state.js
+++ src/store/state.js
@@ -19,6 +19,7 @@ const state = {
   form: [],                     // 설문폼
   donationBox: [],              // 모금함 정보
   mySurveyForm: [],             // 내작성한 모든설문조사 
+  myAlbaForm:[],
   saleSurvey:[],                // 마켓카드정보
   testNumber: null,
   sellSurvey:[],
